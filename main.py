66628d352a47e4e120f7445027bf0250c90a715e
swwind <i@sww.moe>
2023-10-05 11:19:38 +0800

    add some interesting things

--- main.py
+++ main.py
@@ -6,59 +6,120 @@ from recruit import RecruitConfigBox
 from infrast import InfrastConfigBox
 from fight import FightConfigBox
 
-class ItemList(Gtk.ListBox):
-    def __init__(self, detail_panel):
-        Gtk.ListBox.__init__(self)
-        self.set_size_request(200, -1)
-        self.set_selection_mode(Gtk.SelectionMode.SINGLE)
+types = [
+    "StartUp",
+    "CloseDown",
+    "Fight",
+    "Recruit",
+    "Infrast",
+    "Mall",
+    "Award",
+    "Roguelike",
+    "Copilot",
+]
+items = [
+    "启动游戏 - StartUp",
+    "关闭游戏 - CloseDown",
+    "自动刷图 - Fight",
+    "公开招募 - Recruit",
+    "基建换班 - Infrast",
+    "信用商店 - Mall",
+    "任务奖励 - Award",
+    "自动刷肉鸽 - Roguelike",
+    "自动抄作业 - Copilot",
+]
 
-        items = ["Roguelike", "StartUp", "Recruit", "Infrast", "Fight"]
+class CreateTaskDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "创建新任务", parent, 0, Gtk.ButtonsType.NONE)
+        self.add_buttons(Gtk.STOCK_OK, Gtk.ResponseType.OK,
+                         Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        self.set_default_size(954, 518)
+        self.set_border_width(10)
+
+        hbox = Gtk.Box(spacing=10)
+
+        self.right_box = Gtk.Box(spacing=10)
+
+        # 创建一个ListBox，并添加一些项目
+        self.listbox = Gtk.ListBox()
+        self.listbox.set_size_request(200, -1)
+        self.listbox.set_selection_mode(Gtk.SelectionMode.SINGLE)
+
         for item in items:
             row = Gtk.ListBoxRow()
             label = Gtk.Label(label=item)
             row.add(label)
-            self.add(row)
+            self.listbox.add(row)
 
-        self.connect("row-selected", self.on_row_selected)
-        self.detail_panel = detail_panel
+        self.listbox.connect("row-selected", self.on_row_selected)
 
-    def on_row_selected(self, _listbox, row):
-        index = row.get_index()
+        hbox.pack_start(self.listbox, False, False, 0)
+        hbox.pack_start(self.right_box, True, True, 0)
 
-        component = None
-        if index == 0:
-            component = RoguelikeConfigBox()
-        elif index == 1:
-            component = StartUpConfigBox()
-        elif index == 2:
-            component = RecruitConfigBox()
-        elif index == 3:
-            component = InfrastConfigBox()
-        elif index == 4:
-            component = FightConfigBox()
-        if component is not None:
-            children = self.detail_panel.get_children()
+        area = self.get_content_area()
+        area.pack_start(hbox, True, True, 0)
+
+        self.connect('response', self.on_response)
+        self.show_all()
+
+    def on_row_selected(self, _, row):
+        type = types[row.get_index()]
+        print(row.get_index(), type)
+        self.component = None
+
+        if type == "StartUp":
+            self.component = StartUpConfigBox()
+        elif type == "CloseDown":
+            pass
+        elif type == "Fight":
+            self.component = FightConfigBox()
+        elif type == "Recruit":
+            self.component = RecruitConfigBox()
+        elif type == "Infrast":
+            self.component = InfrastConfigBox()
+        elif type == "Mall":
+            pass
+        elif type == "Award":
+            pass
+        elif type == "Roguelike":
+            self.component = RoguelikeConfigBox()
+        elif type == "Copilot":
+            pass
+
+        if self.component is not None:
+            children = self.right_box.get_children()
             for child in children:
-                self.detail_panel.remove(child)
+                self.right_box.remove(child)
                 child.destroy()
-            self.detail_panel.add(component)
-            component.show_all()
+            self.right_box.add(self.component)
+            self.component.show_all()
+
+    def on_response(self, _, response_id):
+        if response_id == Gtk.ResponseType.OK:
+            self.type = types[self.listbox.get_selected_row().get_index()]
+            self.config = self.component.get_config()
 
 class MainWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Item List Demo")
-        self.set_default_size(1280, 720)
-        self.set_border_width(10)
+        Gtk.Window.__init__(self, title="MAA GTK")
 
-        hbox = Gtk.Box(spacing=10)
-        self.add(hbox)
+        button = Gtk.Button(label="点击我啊")
+        button.connect("clicked", self.create_new_task)
+        self.add(button)
 
-        detail_panel = Gtk.Box(spacing=10)
-        detail_panel.add(StartUpConfigBox())
-        item_list = ItemList(detail_panel)
+    def create_new_task(self, button):
+        dialog = CreateTaskDialog(self)
+        response = dialog.run()
 
-        hbox.pack_start(item_list, False, False, 0)
-        hbox.pack_start(detail_panel, True, True, 0)
+        if response == Gtk.ResponseType.OK:
+            print('OK')
+            print(dialog.type)
+            print(dialog.config)
+        elif response == Gtk.ResponseType.CANCEL:
+            print('Cancel')
+
+        dialog.destroy()
 
 win = MainWindow()
 win.connect("destroy", Gtk.main_quit)
